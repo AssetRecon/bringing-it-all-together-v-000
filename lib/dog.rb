2a6cbe523e6b2b4dda893b3edf2483ddf0b067b6
Edsel Caprice <caprice@mail.usf.edu>
2018-08-11 13:08:06 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -11,8 +11,12 @@ class Dog
 
   def self.create_table
     sql = "CREATE TABLE dogs (id INTEGER PRIMARY KEY, name TEXT, breed TEXT)"
-
     DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+    DB[:conn].execute("DROP TABLE dogs")
+
   end
 
 
